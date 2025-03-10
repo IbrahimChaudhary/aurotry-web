0f5ddce3627dd8388ac1c256f90460d83a1cad7f
IbrahimChaudhary <ibrahimchaudhary421@gmail.com>
2025-03-11 00:28:24 +0500

    fix: remove useless imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Work_Sans } from "next/font/google";
+import { Work_Sans } from "next/font/google";
 
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const workSans = Work_Sans({
   variable: "--font-work-sans",
