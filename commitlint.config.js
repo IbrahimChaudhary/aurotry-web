2e946595e066ba8ec436d4049692b563703ea3ca
IbrahimChaudhary <ibrahimchaudhary421@gmail.com>
2025-03-16 19:59:37 +0500

    branch: init

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,6 @@ module.exports = {
       'type-enum': [2, 'always', [
         'build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 
         'refactor', 'revert', 'style', 'test', 'merge', 'branch'
-        // Added 'merge' to the list of allowed types
       ]]
     }
   };
