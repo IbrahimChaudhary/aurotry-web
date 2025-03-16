f36e9aa8ed17c675d207b11763fab790a31e499f
IbrahimChaudhary <ibrahimchaudhary421@gmail.com>
2025-03-16 19:43:12 +0500

    feat: landing page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import BenefitsSection from "@/components/ui/benefits-section";
 import FAQSection from "@/components/ui/faq-section";
 import FeaturesSection from "@/components/ui/features-section";
-import Footer from "@/components/ui/footer";
-import Header from "@/components/ui/header";
+import Footer from "@/components/ui/Footer";
+import Header from "@/components/ui/Header";
 import HeroBlock from "@/components/ui/hero-block";
 import PlansSection from "@/components/ui/plans-section";
 
